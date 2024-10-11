1b442f2da0de5c4f6d6981274d919c793e602836
Mathys <mathystheolade973@gmail.com>
2024-10-12 00:02:11 +0200

    lint

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -3,7 +3,7 @@ import { EnvEnum } from "@config/env";
 // https://stackoverflow.com/questions/45194598/using-process-env-in-typescript
 declare global {
   namespace NodeJS {
-    type ProcessEnv = IProcessEnv;
+    interface ProcessEnv extends IProcessEnv {}
   }
 }
 
