c3f1d8e8bc9a787910b98dbb5d6acf1779fd1194
Mathys <mathystheolade973@gmail.com>
2024-10-18 18:03:00 +0200

    wip/ stubs classes

--- README.md
+++ README.md
@@ -13,3 +13,34 @@ export class TwitterDriver extends BaseDriver {
 }
 ```
 Il doit toujours être utilisé avant le décorateur `@injectable()` car le compilateur TypeScript exécute les décorateurs dans l'ordre inverse. Ainsi, `@bind()` sera exécuté après `@injectable()`, ce qui permet de définir correctement le scope de la classe après son injection.
+
+
+# Tests
+
+## Stubs
+
+
+Here an example of how to stubs a class using jest
+```ts
+// Example of how to sub a class
+test("stub prepareAndTriggerBrightData pour retourner salut", async () => {
+  const brightDataController = container.get(BrightDataController);
+
+  jest
+    .spyOn(brightDataController, "prepareAndTriggerBrightData")
+    .mockResolvedValue({
+      snapshot_id: "123",
+    });
+
+  const result = await brightDataController.prepareAndTriggerBrightData(
+    "instagram_posts",
+    "/endpoint",
+    ["https://example.com"]
+  );
+
+  expect(result).toEqual({
+    snapshot_id: "123",
+  });
+});
+
+```
--- src/__tests__/api/controllers/brighttdata/brightdata.controller.test.ts
+++ src/__tests__/api/controllers/brighttdata/brightdata.controller.test.ts
@@ -6,15 +6,29 @@ IocContainer.initContainer();
 import { BrightDataController } from "@api/controllers/brightdata/brightdata.controller";
 
 const container = IocContainer.container;
-beforeEach(() => {
-  container.snapshot();
-});
-
-afterEach(() => {
-  container.restore();
-});
 
 test("adds 1 + 2 to equal 3", () => {
   const brightDataController = container.get(BrightDataController);
   expect(brightDataController).toBeDefined();
+});
+
+// Example of how to sub a class
+test("stub prepareAndTriggerBrightData pour retourner salut", async () => {
+  const brightDataController = container.get(BrightDataController);
+
+  jest
+    .spyOn(brightDataController, "prepareAndTriggerBrightData")
+    .mockResolvedValue({
+      snapshot_id: "123",
+    });
+
+  const result = await brightDataController.prepareAndTriggerBrightData(
+    "instagram_posts",
+    "/endpoint",
+    ["https://example.com"]
+  );
+
+  expect(result).toEqual({
+    snapshot_id: "123",
+  });
 });
