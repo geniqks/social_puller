1877a27f82e66615190bc2320409ab46b2987e12
Mathys <mathystheolade973@gmail.com>
2024-10-25 01:50:01 +0200

    remove unused controller

--- src/drivers/brightdata/brightdata.module.ts
+++ src/drivers/brightdata/brightdata.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { BrightDataController } from './controllers/brightdata.controller';
 import { BrightdataDriver } from './driver/brightdata.driver';
 import { BrightdataMonitorModule } from './modules/brightdata-monitor/brightdata-monitor.module';
 import { BrightdataService } from './services/brightdata.service';
@@ -7,7 +6,6 @@ import { BrightdataService } from './services/brightdata.service';
 const exported = [BrightdataDriver, BrightdataService];
 
 @Module({
-  controllers: [BrightDataController],
   imports: [BrightdataMonitorModule],
   providers: [...exported],
   exports: [...exported],
--- src/drivers/brightdata/controllers/brightdata.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('brightdata')
-export class BrightDataController {
-  @Get()
-  public async getBrightData(): Promise<string> {
-    return 'salut 2';
-  }
-}
